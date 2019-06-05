ef1511857045ee6878df1a742e3e5bad1bf865c9
Patrik Hermansson <hermansson.patrik@gmail.com>
2019-06-05 22:23:30 +0200

    Fine-tuning

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include "ntp.h"
 #include "settings.h"
 
+char realTime[20], realDate[20];
+int realyear, realmon;
+
 void setup() {
   Serial.begin(serialspeed);
   connectWifi();
@@ -10,14 +13,20 @@ void setup() {
 }
 
 void loop() {
-  time_t now;
-  struct tm * timeinfo;
-  time(&now);
-  timeinfo = localtime(&now);
+  time_t now;                 // Create a variable for time
+  struct tm * timeinfo;       // A struct with time fields
+  time(&now);                 // get current time; same as: now = time(NULL)
+  timeinfo = localtime(&now); // struct tm * localtime (const time_t * timer);
+                              // Convert time_t to tm as local time
 
-  char totTime[20];
-  sprintf_P(totTime, (PGM_P)F("%02d:%02d:%02d"), timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
-  Serial.println(totTime);
+  sprintf_P(realTime, (PGM_P)F("%02d:%02d:%02d"), timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+  realyear = timeinfo->tm_year + 1900;
+  realmon = timeinfo->tm_mon + 1;
+  sprintf_P(realDate, (PGM_P)F("%04d-%02d-%02d"), realyear, realmon, timeinfo->tm_mday);
+
+  Serial.println(realTime);
+  Serial.println(realDate);
+
   delay(1000);
 
 }
